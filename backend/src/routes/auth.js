36559ba1b1ffd4e4d14de755d4086345543e6e80
Matheus Ferreira <mathxtzferreira@gmail.com>
2026-06-23 16:56:45 -0300

    ajuste final

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, registerAdmin, loginAdmin } = require('../controllers/authController');
-const { loginGoogle } = require('../controllers/AuthController');
+const { loginGoogle } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
