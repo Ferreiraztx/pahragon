9f8b7d7ec43401e45de29055831a27d6d443bc8d
Matheus Ferreira <mathxtzferreira@gmail.com>
2026-06-23 16:40:57 -0300

    ajuste login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,25 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* Substitua pelo seu ID real obtido no passo 1 */}
+    {/* 1. O Provedor do Google envolve todo o ecossistema */}
     <GoogleOAuthProvider clientId="51381606864-terprlk1d65qajdnovbr2vlh758vspq2.apps.googleusercontent.com">
-      <App />
+      {/* 2. O BrowserRouter garante que os hooks de rotas (useNavigate, Link) funcionem */}
+      <BrowserRouter>
+        {/* 3. O AuthProvider gerencia o contexto de login do seu sistema */}
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </GoogleOAuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
