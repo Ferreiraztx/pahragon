ab5b36686c5294cbe3239f482ed8f0be17541677
Matheus Ferreira <mathxtzferreira@gmail.com>
2026-06-23 16:59:12 -0300

    ajuste final 1.1

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -7,6 +7,6 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/admin/register', registerAdmin);
 router.post('/admin/login', loginAdmin);
-router.post('/auth/google', loginGoogle);
+router.post('/google', loginGoogle);
 
 module.exports = router;
