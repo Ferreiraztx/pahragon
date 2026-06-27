889d98fa8d9f4b875cf91491f741c5cd07302799
Matheus Ferreira <mathxtzferreira@gmail.com>
2026-06-27 19:18:42 -0300

    Ajuste Login

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -15,14 +15,16 @@ function authMiddleware(req, res, next) {
 
   console.log('TOKEN:', token);
 
-  try {
+try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log('DECODED:', decoded);
-    req.userId = decoded.userId;
+    
+    // Unifica para garantir que o ID sempre chegue no controller, não importa se foi gerado como 'id' ou 'userId'
+    req.userId = decoded.userId || decoded.id; 
     req.user = decoded;
     req.userRole = decoded.role;
     next();
-  } catch (err) {
+} catch (err) {
     console.log('ERRO JWT:', err.message);
     return res.status(401).json({ error: 'Token inválido' });
   }
